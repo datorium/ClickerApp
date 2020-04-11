7118208dbb9db505e1f52804bf2b2ea9d8c70eee
Datorium <hello@datorium.lv>
2020-04-11 17:38:24 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ClickerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FirstButton
+            // 
+            this.FirstButton.BackColor = System.Drawing.Color.Red;
+            this.FirstButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstButton.Location = new System.Drawing.Point(210, 98);
+            this.FirstButton.Name = "FirstButton";
+            this.FirstButton.Size = new System.Drawing.Size(200, 200);
+            this.FirstButton.TabIndex = 0;
+            this.FirstButton.Text = "Click Me";
+            this.FirstButton.UseVisualStyleBackColor = false;
+            this.FirstButton.Click += new System.EventHandler(this.FirstButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FirstButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FirstButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickerApp
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        int fontSize = 20;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstButton_Click(object sender, EventArgs e)
+        {
+            int red = rand.Next(0, 256);
+            int green = rand.Next(0, 256);
+            int blue = rand.Next(0, 256);
+
+            FirstButton.BackColor = Color.Purple;
+            FirstButton.Width -= 5;
+            FirstButton.Height -= 5;
+            FirstButton.Top = rand.Next(0, 300);
+            FirstButton.Left = rand.Next(0, 300);
+            FirstButton.BackColor = Color.FromArgb(red, green, blue);
+            FirstButton.Font = new Font("Arial", fontSize, FontStyle.Bold);
+            fontSize -= 1;
+        }
+    }
+}
